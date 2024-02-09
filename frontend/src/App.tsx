b26fe898a71c3efa71cb935b370d675f75e08690
gtsaroj <sarojgt326@gmail.com>
2024-02-09 20:19:55 +0545

    pending in register & complete card component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Register } from './Components/Register/Register';
 import Login from './Components/Login/Login';
+import Card from './Components/Card/Card';
 
  export const App : React.FC = () => {
   return (
-    <div> <Register/></div>
+    <div> <Card/></div>
   )
 }
 
