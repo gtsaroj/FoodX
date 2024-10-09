0f59c146b1a51511ea38a77f582d6e1800b35d1e
AaaayushXD <aayushlamichhane.hehe@gmail.com>
2024-10-09 14:18:32 +0545

    socket: update path

--- Admin/src/Utility/socket.util.ts
+++ Admin/src/Utility/socket.util.ts
@@ -2,11 +2,12 @@ import { io } from "socket.io-client";
 import Cookie from "js-cookie";
 
 const token = Cookie.get("accessToken");
-const socket = io("http://localhost:5000", {
+const socket = io(process.env.VITE_API_URL, {
   auth: {
     token: token,
   },
+  transports: ["websocket", "polling"],
+  path: "/socket.io",
 });
-
 
 export { socket };
