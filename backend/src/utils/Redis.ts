dba8e0b5396c0c09c48f8da9b4c336d979ee99ad
AaaayushXD <aayushlamichhane.hehe@gmail.com>
2024-10-09 10:20:39 +0545

    redis: connect to onrender

--- backend/src/utils/Redis.ts
+++ backend/src/utils/Redis.ts
@@ -1,9 +1,18 @@
 import { createClient } from "redis";
 
-const redisClient = createClient();
+const redisClient = createClient({
+  url: process.env.REDIS_URL,
+});
 redisClient.on("error", (error) => {
   console.error(`Redis client error:`, error);
 });
-await redisClient.connect();
+await redisClient
+  .connect()
+  .then(() => {
+    console.log("Connected to Redis successfully!");
+  })
+  .catch((err) => {
+    console.error("Failed to connect to Redis:", err);
+  });
 
 export { redisClient };
