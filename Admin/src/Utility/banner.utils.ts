560b1ae77e90d1a50289d1ba19a681ab32a7418c
gtsaroj <sarojgt326@gmail.com>
2024-10-27 14:35:12 +0545

    remove Admin

--- Admin/src/Utility/banner.utils.ts
+++
@@ -1,8 +0,0 @@
-import { BannerModel } from "../models/banner.model";
-
-export const SearchBanner = (categories: BannerModel[], value: string) => {
-    const searchingCategory = categories?.filter((category) => {
-      return category.title?.toLowerCase().includes(value);
-    });
-    return searchingCategory;
-  };
--- Admin/src/models/SlideProp.ts
+++
@@ -1,7 +0,0 @@
-interface SlideProp {
-  url: string;
-}
-
-export interface Slides {
-  slides: SlideProp[];
-}
