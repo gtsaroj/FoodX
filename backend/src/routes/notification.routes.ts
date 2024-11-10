48b2ec34f8ede4dabd70ffdd09e079bac29c9abd
AaaayushXD <aayushlamichhane.hehe@gmail.com>
2024-11-10 19:16:18 +0545

    remove: unused function

--- backend/src/routes/notification.routes.ts
+++ backend/src/routes/notification.routes.ts
@@ -2,7 +2,6 @@ import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middlewares.js";
 import {
   addNotification,
-  bulkDeleteNotificationsFromDatabase,
   deleteNotification,
   fetchNotifications,
 } from "../controllers/notification.controller.js";
@@ -12,8 +11,5 @@ const notificationRouter = Router();
 notificationRouter.route("/add").post(verifyJwt, addNotification);
 notificationRouter.route("/fetch").post(verifyJwt, fetchNotifications);
 notificationRouter.route("/delete").delete(verifyJwt, deleteNotification);
-notificationRouter
-  .route("/bulk-delete")
-  .delete(verifyJwt, bulkDeleteNotificationsFromDatabase);
 
 export { notificationRouter };
