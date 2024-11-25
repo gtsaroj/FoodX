c4383d50e64acb702f40234d2ab9241289a1f03c
gtsaroj <sarojgt326@gmail.com>
2024-11-25 14:05:48 +0545

    add feedback form, dark mode in navbar when use not authenticated

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: "./",
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: "./index.html",
+    },
+  },
   server: {
-    host : "0.0.0.0"
-  }
-})
+    host: "0.0.0.0",
+  },
+});
